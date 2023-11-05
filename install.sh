e11ef034d33120c8ce4f20c8c9a95356e02bc285
PhoenixPars <60186819+PhoenixPars@users.noreply.github.com>
2023-11-05 22:10:34 +0330

    Create install.sh
    add installtion prj-occupied_port

---
+++ install.sh
@@ -0,0 +1,8 @@
+apt update
+apt install python
+apt install git
+git clone https://github.com/PhoenixPars/occupied_port/blob/main/scan.py
+clear
+echo "\033[1;32m install success full..."
+echo "\n"
+echo "\033[1;37m Use the \033[1m python scan.py \033[1;37m command to run the script"
